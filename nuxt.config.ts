6cf33fb8e3f6b22adab872ec84e32be5eeba80ac
sudostrat <minustars1124@yahoo.co.jp>
2024-09-18 19:10:36 +0900

    動的ルーティングを行っているパスの静的ファイル生成を実施する設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import DiscList from './assets/json/discs.json';
+const discList : Array<Disc> = DiscList.items;
+interface Disc {
+  id: string,
+}
+
 export default defineNuxtConfig({
   app: {
     head: {
@@ -12,5 +18,14 @@ export default defineNuxtConfig({
     '/assets/css/reset.css',
     '/assets/css/style.css'
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  hooks: {
+    // 動的ルーティングを行っている disc/[id] の静的ファイル生成を実施
+    async "nitro:config"(nitroConfig) {
+      if (nitroConfig.prerender?.routes == undefined) {
+        return;
+      }
+      nitroConfig.prerender.routes = discList.map(disc => '/disc/' + disc.id);
+    }
+  }
 })
